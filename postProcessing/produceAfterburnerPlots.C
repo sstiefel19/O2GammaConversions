0ab94c5a39e8addb213668392dffd5adc4d294fb
Stephan Stiefelmaier <stiefelmaier@physi.uni-heidelberg.de>
2022-08-01 10:19:56 +0200

    add option to rebin with variable bin size for convprob and efficiency plots

--- postProcessing/produceAfterburnerPlots.C
+++ postProcessing/produceAfterburnerPlots.C
@@ -180,7 +180,153 @@ void plotEfficiencyVspT(std::vector<std::string>& theNomDenFileNames)
 }
 
 
+// function to aling the edges in theXBinsWanted with the ones given by the theHisto. theXBinsGood will contain only edges that are also there in
+// theHisto and that are the same or to the left of the ones in theXBinsWanted
+void fillArrayWithNewEdges(TH1F* theHisto, Double_t *theXBinsWanted, int theNEdgesNew, Double_t *theXBinsGood){
 
+    for (int iNew=0; iNew<theNEdgesNew; ++iNew){
+        double_t lEdgeNew = theHisto->GetBinLowEdge(theHisto->FindBin(theXBinsWanted[iNew]));
+        theXBinsGood[iNew] = lEdgeNew;
+    }
+}
+
+void printArray(Double_t *theArr, int theN){
+
+    cout << "printArray start\n";
+    for (int i =0; i< theN; ++i){
+        cout << i << " " << theArr[i] << endl;
+    }
+    cout << "printArray end\n";
+}
+
+
+/* get an xbins array that can be used to rebin a histogram. specify axis boarders and how many bins in the old binning should be combined into one bin in the new binning for each region between the boarders.
+ * example usage:
+ * const int lNBoarders = 2;
+   double_t lBoarders[lNBoarders] = { 1., 2.,};
+   int lFactors[lNBoarders] = { 4, 6 };
+   // -> use old bins below 1., combine 4 bins into one between 1. and 2., and combine 6 bins into one bin above 2.
+   double_t lXBinsNew[1000];
+int lNBinsNew = computeRebinnedXbins(hNom, lBoarders, lFactors, lNBoarders, lXBinsNew);
+ */
+
+int computeRebinnedXbins(TH1* theHisto, Double_t* theBoarders, int* theFactors, int theNBoarders, Double_t* theXbinsNew){
+
+    int i = 1;
+    int iNew = 0;
+    int iBoarder = 0;
+    int inc = 1;
+    bool  lLastBoarderReached = false;
+
+    while (i <= theHisto->GetNbinsX()){
+
+        theXbinsNew[iNew] = theHisto->GetBinLowEdge(i);
+        ++iNew;
+
+        if ( !lLastBoarderReached && theHisto->GetBinLowEdge(i+1) > theBoarders[iBoarder]){
+
+            inc = theFactors[iBoarder];
+            if (iBoarder+1 < theNBoarders){
+                ++iBoarder;
+            }
+            else {
+                lLastBoarderReached = true;
+            }
+        }
+        i += inc;
+    }
+    return iNew-2;
+}
+
+void printHistoEdges(TH1 *theHisto){
+    cout << "printHistoEdges start\n";
+
+    int nBins = theHisto->GetNbinsX();
+    for(int i=1; i<=nBins; ++i){
+        cout << i << " " << theHisto->GetBinLowEdge(i) << endl;
+    }
+    cout << nBins << " " << theHisto->GetBinLowEdge(nBins+1) << endl;
+    cout << "printHistoEdges end\n";
+}
+
+
+void plotConversionProbability(std::vector<std::string>& theNomDenFileNames){
+
+    std::string lNamePDF("plot-conversion-probabilty.pdf");
+    std::vector<std::string> lNomDenHistoPaths{
+        { "gamma-conversions-truth-only-mc/hGammaConvertedRP_MCTrue" },
+        { "gamma-conversions-truth-only-mc/hGammaProdAfterCutsP_MCTrue" },
+    };
+
+    std::vector<TObject*> lNomDenObjects;
+    for (auto i : util::lang::indices(lNomDenHistoPaths)){
+        lNomDenObjects.push_back(getObjectFromPathInFile(theNomDenFileNames[1],lNomDenHistoPaths[i]));
+    }
+
+    TH2F* hNomRP = (TH2F*)lNomDenObjects[0];
+
+    float lRMinWanted = 5.0;
+    float lRMaxWanted = 180.;
+
+    TAxis* lXaxis = hNomRP->GetXaxis();
+
+    int iBinMin = lXaxis->FindBin(lRMinWanted);
+    int iBinMax = lXaxis->FindBin(lRMaxWanted) - 1;
+
+    float lRMinIs = lXaxis->GetBinLowEdge(iBinMin);
+    float lRMaxIs = lXaxis->GetBinUpEdge(iBinMax);
+
+    cout << "will integrate in R between " << lRMinIs <<  " and " << lRMaxIs << endl;
+
+    TH1* hNomP = (TH1D*)hNomRP->ProjectionY("hGammaConvertedP_fromproj", iBinMin, iBinMax);
+
+    std::vector<TH1*> lNomDenHistos;
+
+    lNomDenHistos.push_back(hNomP);
+    lNomDenHistos.push_back((TH1*)lNomDenObjects[1]);
+
+
+    printHistoEdges(hNomP);
+
+    // define new binning
+    const int lNBoarders = 5;
+    double_t lBoarders[lNBoarders] = { 1., 2., 3., 4., 8.};
+    int lFactors[lNBoarders] = { 4, 6, 8, 24, 128 };
+    double_t lXBinsNew[1000];
+
+    int lNBinsNew = computeRebinnedXbins(hNomP, lBoarders, lFactors, lNBoarders, lXBinsNew);
+
+    printArray(lXBinsNew, lNBinsNew);
+
+    for (auto& histo : lNomDenHistos){
+        histo = dynamic_cast<TH1*>(histo->Rebin(lNBinsNew, (std::string(histo->GetName()) + "_reb").data(), lXBinsNew));
+    }
+
+
+
+    /*
+    double_t xBinsWanted[4] = {0.0, 1.01, 4.0, 8.0 };
+    double_t xBinsFitting[4];
+    int nBinsNew = 3;
+
+    TH1F* hNom = lNomDenHistos[0];
+
+    printArray(xBinsWanted, 4);
+
+    fillArrayWithNewEdges(hNom, xBinsWanted, 4, xBinsFitting);
+
+    printArray(xBinsFitting, 4);
+    */
+
+
+
+
+    TCanvas *lCanvas = plotRatioWithTEfficiency(lNomDenHistos, nullptr, 1, "conversion probability run3 from prim. photons (acc. alice def.) with eta<0.8 ", "p (GeV)", "p_{conv.}", 0.6);
+    if (lCanvas){
+        lCanvas->SaveAs(lNamePDF.data());
+    }
+
+}
 
 void produceAfterburnerPlots(std::string theFnameNom="", std::string theFnameDen=""){
     gROOT->Reset();
@@ -188,26 +334,11 @@ void produceAfterburnerPlots(std::string theFnameNom="", std::string theFnameDen
     std::vector<std::string> lNomDenFileNames{theFnameNom, theFnameDen=="" ? theFnameNom : theFnameDen};
 
     // convprob plot
-    //~ {
-        //~ std::string lNamePDF("plot-conversion-probabilty.pdf");
-        //~ std::vector<std::string> lNomDenHistoPaths{
-            //~ { "gamma-conversions-truth-only-mc/hGammaConvertedP_Rsel_MCTrue" },
-            //~ { "gamma-conversions-truth-only-mc/hGammaProdAfterCutsP_MCTrue" },
-        //~ };
 
-        //~ std::vector<TH1F*> lNomDenHistos;
-        //~ for (auto i : util::lang::indices(lNomDenHistoPaths)){
-            //~ lNomDenHistos.push_back((TH1F*)getObjectFromPathInFile(lNomDenFileNames[1],lNomDenHistoPaths[i]));
-        //~ }
-
-        //~ TCanvas *lCanvas = plotRatioWithTEfficiency(lNomDenHistos, nullptr, 1, "conversion probability run3 from prim. photons (acc. alice def.) with eta<0.8 ", "p (GeV)", "p_{conv.}", 0.6);
-        //~ if (lCanvas){
-            //~ lCanvas->SaveAs(lNamePDF.data());
-        //~ }
-    //~ }
+    plotConversionProbability(lNomDenFileNames);
 
 
 
     //~ plotEfficiencyVsR(lNomDenFileNames);
-    plotEfficiencyVspT(lNomDenFileNames);
+    //plotEfficiencyVspT(lNomDenFileNames);
 }
